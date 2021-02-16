a5056e1920f4f64a8c81eb85ef1f8c0925efc056
prab-cyber <prab@cowbellcyber.ai>
2021-02-15 18:36:52 -0800

    added moment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import moment from "moment";
 import "./App.css";
 
 function App() {
+  const date = moment().format('MM/DD/YYYY hh:mm');
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Welcome To Telestrations</p>
-        <p>by Nishloka and Aayush</p>
+        <p>
+          <div>by Nishloka and Aayush</div>
+          <div>{date}</div>
+          </p>
+
       </header>
     </div>
   );
