97183b176ba9313cc153353c55b55fb8bdca7e2b
prab-cyber <prab@cowbellcyber.ai>
2021-02-18 21:04:23 -0800

    public folder changed to docs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import moment from "moment";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const date = moment().format('MM/DD/YYYY hh:mm');
+  const date = moment().format('MM/DD/YYYY hh:mm:ss');
+  const [now, setNow] = useState(date);
+
+  setTimeout( ()=> {
+    const date = moment().format('MM/DD/YYYY hh:mm:ss');
+    setNow(date);
+  }, 1000)
 
   return (
     <div className="App">
@@ -10,7 +17,7 @@ function App() {
         <p>Welcome To Telestrations</p>
         <p>
           <div>by Nishloka and Aayush</div>
-          <div>{date}</div>
+          <div>{now}</div>
           </p>
 
       </header>
